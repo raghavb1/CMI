99499e5ed7aefe346353e8918e0308f081225def
Deept Raghav <raghavb1@gmail.com>
2016-05-28 20:22:12 +0530

    Create overseer_setup.sh

---
+++ overseer_setup.sh
@@ -0,0 +1,34 @@
+apt-get -y install nodejs npm zip unzip nodejs-legacy
+
+cat <<EOF >> oval_key.pem
+-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEAtN1nuXVA8krDzh4FTozVyA8EcFOYtpfasECDf1D6BzOfZFdNswWMF/ffvRBX
+D88bgPxcs2BZI58wERswomsC+NFm145vlGQBXc5tywwAdkmoNOngos6jDcVrlQ56uGEi4uNJl7u8
+9PqyBWvypdVYb5hkp+B9lLl93dEhDPCQSm4sWTcW3UEuVLdeMdlw9zkSOrS9wfhbzYWGqnPPJ978
+41GxVQrF20XQDUn4AyL9DkKPyognVDs8vXOnySWEOrW0BlP3Vym3dJhAGtFdWQ1EimWQ0qSAqL2E
+HaW52Co/hdRTR30odtjqRN467OZpN08JXKuCS0RX4mTJ8lNVS68ikwIDAQABAoIBAQC0uKrlIGl1
+8Qj4Ev1AgO84iPDpgE6O3OuSw9PhSruaqJVzAN1NrZRPesngs8waqtqTpxbF0dcgBfdUkHOnRwBg
+OXTmnJeXkdnfrt38TpDDoVPnE273nzxEDWkzNpWfWMgwJ+YoLFph+4IYcsWxuJ02XPLu4Bz7l+FR
+3J6GvHWhOAkmbNOn4KfbzMwTPzKCPOzd8AsXr0WCUZA1/Z1JrJi17kygPSEkU9Auv18booafEGUP
+jj5nb8wbSNetGP3duhvQZgrQ7IsLrmHWwVRlL9bH6mBN2vs3Ou0+SW4+bFxSu4AQYQCaoJZ/Koyw
+PYkzhWolHQDg2mjeiwj/Ax47nshJAoGBANixy1XC5DvVaNqMBM6zwK31OwIEt/1zZg0mFNkhwEF8
+L5LVCg7c8hiQFCZ915aT4UTRbzwTAugebaZD3oXP2lnN3JRG2Oo8AIcH+ndZLtqszbWeOB0N+1uA
+9bKUeo7ZeT4VGNtRkgc59rtvcXZ7lsjrxRDC8UG8POTpAz140rINAoGBANWr3KmD9awv2Acq/OOH
+CWsO9vaoQtKls3V4AqxcJ/zcpYUe6J3P2oDUwrS7jZNnvZA68bt9BgsPSWv9jElCDD9rnrivaCLm
+b+2gK+LHtf3YUaX/l5QjNzN0omADg00VbDDPA2/dkGiQwB3KNANa+Vy/AtszhOdZ4UtuqiuGDB8f
+AoGAaL+JSyuqqEHBLeQBbun1eiHRJGijiCEAc9q0uUFXblBZruDMu+KSJM2A8Bpk3KUff+S9oIyQ
+GySaXITyTDztj/uzZPnaYWAf4SY6LPcvbwWZavHQrjrUBqeQHYMou2Tk9t275kjIDjY2zuRQNLYJ
+bZaK7E9P0DbuOLlql4yQTQECgYAFsUh1s7BN4BBvUHPgU+6qTYHC3IS7O/LmBEZ99Q6TrAU04Lft
+zGXT3Nc7HWwOK0tfllJuXkxU6xlXqS+dnAbbgbB+1x19IIqG2CeTKSLuGl9Cfua46Z9E3aydxjov
+SYzSWBGNX2fDgWe843AzTq8qC2S2Fk9KIpjf+5jJBA86nQKBgE5zo4rc4edXo6azGPHvfddd0C8J
+sPX0RtefTAXX9ty11AY72brfsssMuZHZSo1rdm5srdwokSOn+VYXQx58Zj3nvXTb4+uOvTVCT+UO
+xXYOsudR5+4rVuoYedud5Czu69nQCBZ5cniEWcCTYglB7DIswbY8FMlm7dHBldBsrrc6
+-----END RSA PRIVATE KEY-----
+EOF
+
+chmod 400 oval_key.pem
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
