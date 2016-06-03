34f8d8d9320d88d468d81c371534903cc904a014
Deept Raghav <raghavb1@gmail.com>
2016-06-03 23:09:36 +0530

    Update user_server_setup.sh

--- user_server_setup.sh
+++ user_server_setup.sh
@@ -67,7 +67,16 @@ virsh  net-destroy svmp
 virsh net-create network_config.xml 
 
 virt-install -n svmp_vbox -r 2000 --os-type=linux --disk svmp_system_disk.qcow2,format=qcow2,device=disk,bus=virtio -w bridge=virbr100,model= --vnc --noautoconsole --import --vcpus 2 --hvm  --accelerate
+
+
 virsh destroy svmp_vbox
-virsh start svmp_vbox
+virsh undefine svmp_vbox
+virsh  net-destroy default
+virsh  net-destroy svmp
+
+virsh net-create network_config.xml 
+
+virt-install -n svmp_vbox -r 2000 --os-type=linux --disk svmp_system_disk.qcow2,format=qcow2,device=disk,bus=virtio -w bridge=virbr100,model= --vnc --noautoconsole --import --vcpus 2 --hvm  --accelerate
+
 
 echo KVM > KVM
