df2486a0a3d8bfff090b88c900bb341437da8afe
Deept Raghav <raghavb1@gmail.com>
2016-06-03 22:28:56 +0530

    Update attach_user_disk.sh

--- attach_user_disk.sh
+++ attach_user_disk.sh
@@ -16,6 +16,21 @@ EOF
 
 virsh attach-device svmp_vbox data_disk.xml
 
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+
+sudo iptables -t nat -F
+sudo iptables -t mangle -F
+sudo iptables -F
+sudo iptables -X
+
+sudo iptables -A PREROUTING -t nat -i bond0 -p tcp --dport 8001 -j DNAT --to 192.168.122.58:8001
+
+sudo iptables -A FORWARD -p tcp -d 192.168.122.58 --dport 8001 -j ACCEPT
+
+sudo iptables -t nat -A POSTROUTING -o bond0 -j MASQUERADE
+
 echo USERS > USERS
 
 
