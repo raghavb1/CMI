ae29ba672ab434d8e0cbd679b8f57e7dd9a45540
Deept Raghav <raghavb1@gmail.com>
2016-06-08 00:39:10 +0530

    Update attach_user_disk.sh

--- attach_user_disk.sh
+++ attach_user_disk.sh
@@ -37,8 +37,6 @@ cat <<EOF > data_disk.xml
     </disk>
 EOF
 
-sleep 5s
-
 virsh destroy svmp_vbox
 virsh undefine svmp_vbox
 virsh  net-destroy default
@@ -51,30 +49,20 @@ virsh attach-device svmp_vbox data_disk.xml
 
 
 
-sudo iptables -P INPUT ACCEPT
-sudo iptables -P FORWARD ACCEPT
-sudo iptables -P OUTPUT ACCEPT
+# sudo iptables -P INPUT ACCEPT
+# sudo iptables -P FORWARD ACCEPT
+# sudo iptables -P OUTPUT ACCEPT
 
-sudo iptables -t nat -F
-sudo iptables -t mangle -F
-sudo iptables -F
-sudo iptables -X
+# sudo iptables -t nat -F
+# sudo iptables -t mangle -F
+# sudo iptables -F
+# sudo iptables -X
 
-sudo iptables -A PREROUTING -t nat -i bond0 -p tcp --dport 8001 -j DNAT --to 192.168.122.58:8001
+# sudo iptables -A PREROUTING -t nat -i bond0 -p tcp --dport 8001 -j DNAT --to 192.168.122.58:8001
 
-sudo iptables -A FORWARD -p tcp -d 192.168.122.58 --dport 8001 -j ACCEPT
+# sudo iptables -A FORWARD -p tcp -d 192.168.122.58 --dport 8001 -j ACCEPT
 
-sudo iptables -t nat -A POSTROUTING -o bond0 -j MASQUERADE
-
-virsh destroy svmp_vbox
-virsh undefine svmp_vbox
-virsh  net-destroy default
-virsh  net-destroy svmp
-
-virsh net-create network_config.xml 
-
-virt-install -n svmp_vbox -r 6000 --os-type=linux --disk svmp_system_disk.qcow2,format=qcow2,device=disk,bus=virtio -w bridge=virbr100,model= --vnc --noautoconsole --import --vcpus 2 --hvm  --accelerate
-virsh attach-device svmp_vbox data_disk.xml
+# sudo iptables -t nat -A POSTROUTING -o bond0 -j MASQUERADE
 
 
 echo USERS > USERS
