965dc1723bc95cf61e38ac272749789eeea84484
Deept Raghav <raghavb1@gmail.com>
2016-06-08 00:02:23 +0530

    Update attach_user_disk.sh

--- attach_user_disk.sh
+++ attach_user_disk.sh
@@ -63,6 +63,65 @@ sudo iptables -A FORWARD -p tcp -d 192.168.122.58 --dport 8001 -j ACCEPT
 
 sudo iptables -t nat -A POSTROUTING -o bond0 -j MASQUERADE
 
+
+
+cat <<EOF > network_config.xml
+<network>
+  <name>svmp</name>
+  <forward mode='nat'>
+    <nat>
+      <port start='1024' end='65535'/>
+    </nat>
+  </forward>
+
+  <bridge name='virbr100'  stp='on' delay='0'/>
+  <ip address='192.168.122.1' netmask='255.255.255.0'>
+    <dhcp>
+      <range start='192.168.122.58' end='192.168.122.58'/>
+    </dhcp>
+  </ip>
+</network>
+EOF
+
+virsh destroy svmp_vbox
+virsh undefine svmp_vbox
+virsh  net-destroy default
+virsh  net-destroy svmp
+
+virsh net-create network_config.xml 
+
+cat <<EOF > data_disk.xml
+<disk type='file' device='disk'>
+      <driver name='qemu' type='qcow2'/>
+      <source file='/root/$1.qcow2'/>
+      <target dev='vdb' bus='virtio'/>   
+      <alias name='virtio-disk1'/>
+    </disk>
+EOF
+
+sleep 3s
+
+virt-install -n svmp_vbox -r 6000 --os-type=linux --disk svmp_system_disk.qcow2,format=qcow2,device=disk,bus=virtio -w bridge=virbr100,model= --vnc --noautoconsole --import --vcpus 2 --hvm  --accelerate
+virsh attach-device svmp_vbox data_disk.xml
+
+#sleep 10s
+
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+
+sudo iptables -t nat -F
+sudo iptables -t mangle -F
+sudo iptables -F
+sudo iptables -X
+
+sudo iptables -A PREROUTING -t nat -i bond0 -p tcp --dport 8001 -j DNAT --to 192.168.122.58:8001
+
+sudo iptables -A FORWARD -p tcp -d 192.168.122.58 --dport 8001 -j ACCEPT
+
+sudo iptables -t nat -A POSTROUTING -o bond0 -j MASQUERADE
+
+
 echo USERS > USERS
 
 
