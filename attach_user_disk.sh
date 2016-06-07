fbb50b9d44e7452e3abd94363fb6b66d809fc488
Deept Raghav <raghavb1@gmail.com>
2016-06-08 00:35:15 +0530

    Update attach_user_disk.sh

--- attach_user_disk.sh
+++ attach_user_disk.sh
@@ -9,21 +9,6 @@ fi
 scp -oStrictHostKeyChecking=no -i central_server_key.pem root@10.99.109.129:gold_images/svmp_data_disk.qcow2 raghavb1@gmail.com.qcow2
 
 
-sudo iptables -P INPUT ACCEPT
-sudo iptables -P FORWARD ACCEPT
-sudo iptables -P OUTPUT ACCEPT
-
-sudo iptables -t nat -F
-sudo iptables -t mangle -F
-sudo iptables -F
-sudo iptables -X
-
-sudo iptables -A PREROUTING -t nat -i bond0 -p tcp --dport 8001 -j DNAT --to 192.168.122.58:8001
-
-sudo iptables -A FORWARD -p tcp -d 192.168.122.58 --dport 8001 -j ACCEPT
-
-sudo iptables -t nat -A POSTROUTING -o bond0 -j MASQUERADE
-
 cat <<EOF > network_config.xml
 <network>
   <name>svmp</name>
@@ -52,6 +37,8 @@ cat <<EOF > data_disk.xml
     </disk>
 EOF
 
+sleep 5s
+
 virsh destroy svmp_vbox
 virsh undefine svmp_vbox
 virsh  net-destroy default
@@ -62,6 +49,22 @@ virsh net-create network_config.xml
 virt-install -n svmp_vbox -r 6000 --os-type=linux --disk svmp_system_disk.qcow2,format=qcow2,device=disk,bus=virtio -w bridge=virbr100,model= --vnc --noautoconsole --import --vcpus 2 --hvm  --accelerate
 virsh attach-device svmp_vbox data_disk.xml
 
+
+
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+
+sudo iptables -t nat -F
+sudo iptables -t mangle -F
+sudo iptables -F
+sudo iptables -X
+
+sudo iptables -A PREROUTING -t nat -i bond0 -p tcp --dport 8001 -j DNAT --to 192.168.122.58:8001
+
+sudo iptables -A FORWARD -p tcp -d 192.168.122.58 --dport 8001 -j ACCEPT
+
+sudo iptables -t nat -A POSTROUTING -o bond0 -j MASQUERADE
 
 echo USERS > USERS
 
