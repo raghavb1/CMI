11c3151419b6018d3bccf704ab541a7d9676332b
Deept Raghav <raghavb1@gmail.com>
2016-06-08 17:34:26 +0530

    Update user_server_setup.sh

--- user_server_setup.sh
+++ user_server_setup.sh
@@ -41,51 +41,9 @@ chmod 400 central_server_key.pem
 
 scp -oStrictHostKeyChecking=no -i central_server_key.pem root@10.99.109.129:gold_images/svmp_system_disk.qcow2 .
 
-cat <<EOF > network_config.xml
-<network>
-  <name>svmp</name>
-  <forward mode='nat'>
-    <nat>
-      <port start='1024' end='65535'/>
-    </nat>
-  </forward>
-
-  <bridge name='virbr100'  stp='on' delay='0'/>
-  <ip address='192.168.122.1' netmask='255.255.255.0'>
-    <dhcp>
-      <range start='192.168.122.58' end='192.168.122.58'/>
-    </dhcp>
-  </ip>
-</network>
-EOF
-
-virsh destroy svmp_vbox
-virsh undefine svmp_vbox
-virsh  net-destroy default
-virsh  net-destroy svmp
-
-virsh net-create network_config.xml 
-
-virt-install -n svmp_vbox -r 6000 --os-type=linux --disk svmp_system_disk.qcow2,format=qcow2,device=disk,bus=virtio -w bridge=virbr100,model= --vnc --noautoconsole --import --vcpus 2 --hvm  --accelerate
-
-
 # virt-install -n svmp_vbox -r 2000 --os-type=linux --disk svmp_system_disk.qcow2,format=qcow2,device=disk,bus=virtio -w bridge=virbr100,model= --vnc --noautoconsole --import --vcpus 2 --hvm  --accelerate
 #virsh attach-device svmp_vbox data_disk.xml
 #sleep 60s
 
-sudo iptables -P INPUT ACCEPT
-sudo iptables -P FORWARD ACCEPT
-sudo iptables -P OUTPUT ACCEPT
-
-sudo iptables -t nat -F
-sudo iptables -t mangle -F
-sudo iptables -F
-sudo iptables -X
-
-sudo iptables -A PREROUTING -t nat -i bond0 -p tcp --dport 8001 -j DNAT --to 192.168.122.58:8001
-
-sudo iptables -A FORWARD -p tcp -d 192.168.122.58 --dport 8001 -j ACCEPT
-
-sudo iptables -t nat -A POSTROUTING -o bond0 -j MASQUERADE
 
 echo KVM > KVM
